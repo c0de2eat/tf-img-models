76bbc5e92a51b8be8d76f111326b70ab2b7f495c
c0de2eat <89120933+c0de2eat@users.noreply.github.com>
2021-09-23 08:50:23 +0800

    Buffer size for shuffle.

--- tfim/data/utils.py
+++ tfim/data/utils.py
@@ -5,10 +5,10 @@ __all__ = ["batchify"]
 
 
 def batchify(
-    dataset: tf.data.Dataset, batch_size: int, *, shuffle: bool
+    dataset: tf.data.Dataset, batch_size: int, *, shuffle: bool, buffer_size: int = None
 ) -> tf.data.Dataset:
     if shuffle:
-        dataset = dataset.shuffle(len(dataset))
+        dataset = dataset.shuffle(buffer_size)
     dataset = dataset.batch(batch_size, num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
