b33825b39b2bb1fa7b46519200ecea2d131cd503
c0de2eat <89120933+c0de2eat@users.noreply.github.com>
2021-11-27 20:42:07 +0800

    Update

--- scripts/cifar.py
+++ scripts/cifar.py
@@ -15,19 +15,19 @@ from tensorflow.keras.layers import GlobalAvgPool2D
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.mixed_precision import global_policy
 from tensorflow.keras.utils import plot_model
-import tensorflow_datasets as tfds
 from tfim import setup_tf, CosineDecayWithWarmup
 from tfim.data import batchify
+from tfim.data import datasets
 from tfim.modeling import backbones
 from tfim.modeling.layers import Dense
 
 
 os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
 os.environ["TF_DETERMINISTIC_OPS"] = "1"
-random.seed(1234)
-np.random.seed(1234)
-tf.random.set_seed(1234)
-rng = tf.random.Generator.from_seed(1234)
+random.seed(253113)
+np.random.seed(253113)
+tf.random.set_seed(253113)
+rng = tf.random.Generator.from_seed(253113)
 
 
 def main(args):
@@ -44,10 +44,16 @@ def main(args):
     os.makedirs(weights_dir, exist_ok=True)
     weights_name = os.path.join(weights_dir, "weights")
 
+    train_dataset, val_dataset, names = datasets.cifar10(args.data_dir)
+
     img_pad_target_h = int(args.img_height * 1.25)
     img_pad_target_w = int(args.img_width * 1.25)
     img_pad_offset_h = (img_pad_target_h - args.img_height) // 2
     img_pad_offset_w = (img_pad_target_w - args.img_width) // 2
+
+    """
+    Add PCA noise with a coefficient sampled from normal distribution N(0,0.1)
+    """
 
     def process_train(img, label):
         img = tf.image.pad_to_bounding_box(
@@ -65,22 +71,25 @@ def main(args):
         img = tf.image.stateless_random_flip_left_right(
             img, seed=rng.uniform_full_int((2,), dtype=tf.int32)
         )
+        # img = tf.image.stateless_random_brightness(
+        #     img, 0.4, seed=rng.uniform_full_int((2,), dtype=tf.int32)
+        # )
+        # img = tf.image.stateless_random_hue(
+        #     img, 0.4, seed=rng.uniform_full_int((2,), dtype=tf.int32)
+        # )
+        # img = tf.image.stateless_random_saturation(
+        #     img, 0.6, 1.4, seed=rng.uniform_full_int((2,), dtype=tf.int32)
+        # )
+        img = tf.image.resize_with_pad(img, args.img_height, args.img_width)
         img = tf.divide(tf.cast(img, tf.float32), 255.0)
-        label = tf.one_hot(label, 10, dtype=tf.float32)
+        label = tf.one_hot(label, len(names), dtype=tf.float32)
         return img, label
 
     def process_val(img, label):
         img = tf.image.resize_with_pad(img, args.img_height, args.img_width)
         img = tf.divide(tf.cast(img, tf.float32), 255.0)
-        label = tf.one_hot(label, 10, dtype=tf.float32)
+        label = tf.one_hot(label, len(names), dtype=tf.float32)
         return img, label
-
-    train_dataset, val_dataset = tfds.load(
-        "cifar10",
-        split=["train", "test"],
-        data_dir=args.data_dir,
-        as_supervised=True,
-    )
 
     train_dataset = train_dataset.map(process_train)
     val_dataset = val_dataset.map(process_val, tf.data.AUTOTUNE)
@@ -88,9 +97,9 @@ def main(args):
         train_dataset,
         args.batch_size,
         shuffle=True,
-        buffer_size=len(train_dataset),
+        buffer_size=len(train_dataset) // 5,
     )
-    val_loader = batchify(val_dataset, args.batch_size, shuffle=False)
+    val_loader = batchify(val_dataset, args.batch_size)
 
     s = (
         "---------------------------------------------------------\n"
@@ -114,7 +123,7 @@ def main(args):
         args.weight_decay,
         CosineDecayWithWarmup(
             args.learning_rate,
-            warmup_epochs=args.epochs // 10,
+            warmup_epochs=5,
             total_epochs=args.epochs,
             steps_per_epoch=len(train_loader),
         ),
@@ -129,7 +138,7 @@ def main(args):
         features = GlobalAvgPool2D()(feature_map)
         if args.dropout > 0:
             features = tf.keras.layers.Dropout(args.dropout)(features)
-        outputs = Dense(10, name="classifier")(features)
+        outputs = Dense(len(names), name="classifier")(features)
         model = Model(inputs=inputs, outputs=outputs)
         model.compile(
             optimizer=optimizer, loss=loss_fn, metrics=["accuracy"],
@@ -177,7 +186,7 @@ def main(args):
     with strategy.scope():
         model.load_weights(weights_name)
     model.save(model_dir, include_optimizer=False)
-    model.evaluate(val_loader)
+    metrics = model.evaluate(val_loader)
 
     predictions, labels = [], []
     for img, label in val_loader:
@@ -188,6 +197,7 @@ def main(args):
         predictions.extend(outputs)
     report = classification_report(labels, predictions)
     with open(os.path.join(args.out_dir, "report.txt"), "w+") as f:
+        f.write(str(metrics) + "\n")
         f.write(str(report))
 
 
@@ -271,7 +281,7 @@ if __name__ == "__main__":
     # Training
     # ==========================================================================
     parser.add_argument(
-        "-e", "--epochs", default=50, type=int, help="Total training epochs."
+        "-e", "--epochs", default=100, type=int, help="Total training epochs."
     )
     parser.add_argument(
         "--gpus",
